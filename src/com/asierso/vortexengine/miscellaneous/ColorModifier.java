73750cb51d71f6251a74c1b31c0da64e1cf2d362
33327643+Asierso@users.noreply.github.com <33327643+Asierso@users.noreply.github.com>
2024-02-01 12:33:41 +0100

    ColorModifier parser

--- src/com/asierso/vortexengine/miscellaneous/ColorModifier.java
+++ src/com/asierso/vortexengine/miscellaneous/ColorModifier.java
@@ -1,10 +1,12 @@
 package com.asierso.vortexengine.miscellaneous;
 
+import org.jsfml.graphics.Color;
+
 /**
  * Color that can be modified in execution time. Using modifier controllers
  * @author Asierso
  */
-public class ColorModifier {
+public final class ColorModifier {
     //Red value
     public int r;
     
@@ -29,5 +31,17 @@ public class ColorModifier {
         this.g = g;
         this.b = b;
         this.a = a;
+    }
+    
+    /**
+     * Parse ColorModifier to normal SFML color (set rgba values in treshold of 0-255)
+     * @return SFML color
+     */
+    public Color getParsedColor(){
+        r = (r < 0? 0 : r > 255? 255 : r);
+        g = (g < 0? 0 : g > 255? 255 : g);
+        b = (b < 0? 0 : b > 255? 255 : b);
+        a = (a < 0? 0 : a > 255? 255 : a);
+        return new Color(r,g,b,a);
     }
 }
